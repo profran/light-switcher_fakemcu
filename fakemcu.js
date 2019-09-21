b0143721a8775cc9ddd5fa0d1e30dc1e0adf5a0b
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-09-21 18:37:08 -0300

    Rename fake topic

--- fakemcu.js
+++ fakemcu.js
@@ -4,14 +4,14 @@ const client = mqtt.connect('mqtt://localhost:1883')
 let on = false
 
 client.on('connect', function () {
-  client.subscribe('1234')
+  client.subscribe('lights/1234')
 })
 
 client.on('message', function (topic, message) {
   const date = new Date()
   console.log(date.toTimeString())
   console.log(`   Incoming message: ${topic} - ${message}`)
-  if (topic.toString() === '1234' && message.toString() !== 'error') {
+  if (topic.toString() === 'lights/1234' && message.toString() !== 'error') {
     console.log('   Changing state...')
     on = message.toString() === '1'
   }
@@ -19,5 +19,5 @@ client.on('message', function (topic, message) {
 })
 
 setInterval(() => {
-  client.publish('1234', on ? '1' : '0')
+  client.publish('lights/1234', on ? '1' : '0')
 }, 1000)
