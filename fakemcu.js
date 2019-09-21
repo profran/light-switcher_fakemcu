0f8e67a050a00c0717b298caedf8859b7cce8d98
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-09-21 17:01:59 -0300

    Add main logic

---
+++ fakemcu.js
@@ -0,0 +1,24 @@
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://localhost:1883')
+
+let on = false
+
+client.on('connect', function () {
+  client.subscribe('1234')
+})
+
+client.on('message', function (topic, message) {
+  const date = new Date()
+  console.log(date.toTimeString())
+  console.log(`   Incoming message: ${topic} - ${message}`)
+  if (topic.toString() === '1234' && message.toString() !== 'error') {
+    console.log('   Changing state...')
+    on = message.toString() === '1'
+    client.publish('1234/state', on ? 'On' : 'Off')
+  }
+  console.log(`   State: ${on}`)
+})
+
+setInterval(() => {
+  client.publish('1234/state', on ? '1' : '0')
+}, 1000)
